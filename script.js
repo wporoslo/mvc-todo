e1f58b0a9d4acf5a294ef0c2b8f03a9d8b92c545
Wojciech Porosło <wporoslo@gmail.com>
2022-03-10 14:37:08 +0100

    event handlers

--- script.js
+++ script.js
@@ -114,7 +114,29 @@ class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
+
+    this.onTodoListChanged(this.model.todos);
   }
+
+  onTodoListChanged = (todos) => {
+    this.view.displayTodos(todos);
+  };
+
+  handleAddTodo = (todoText) => {
+    this.model.addTodo(todoText);
+  };
+
+  handleEditTodo = (id, todoText) => {
+    this.model.editTodo(id, todoText);
+  };
+
+  handleDeleteTodo = (id) => {
+    this.model.deleteTodo(id);
+  };
+
+  handleToggleTodo = (id) => {
+    this.model.toggleTodo(id);
+  };
 }
 
 const app = new Controller(new Model(), new View());
