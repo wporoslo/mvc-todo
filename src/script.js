8600ac20ff6dfb3eb2f5b5517c9a447ae061b53d
Wojciech Porosło <wporoslo@gmail.com>
2022-03-14 09:08:40 +0100

    fix vite config and paths

--- src/script.js
+++ src/script.js
@@ -230,11 +230,8 @@ window.addEventListener('load', () => {
 async function registerSW() {
   if ('serviceWorker' in navigator) {
     try {
-      await navigator
-            .serviceWorker
-            .register('./serviceworker.js');
-    }
-    catch (e) {
+      await navigator.serviceWorker.register('./serviceworker.js');
+    } catch (e) {
       console.log('SW registration failed');
     }
   }
